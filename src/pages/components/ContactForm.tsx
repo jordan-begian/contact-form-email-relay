d7da3c90cd0aadb7cfb99c8090fa2a8e7c4de3e9
Jordan Begian <40347351+jordan-begian@users.noreply.github.com>
2025-05-30 16:51:37 -0400

    add frontend error handling and styling

--- src/pages/components/ContactForm.tsx
+++ src/pages/components/ContactForm.tsx
@@ -1,5 +1,5 @@
 import { ContactFormFields } from '@/shared/models/ContactFormFields';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FormField from './FormField';
 import client from '../services/MessageRelayClient';
 
@@ -10,21 +10,49 @@ const ContactForm: React.FC = () => {
     contactInfo: '',
     message: '',
   });
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [visibleError, setVisibleError] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    if (error) {
+      setVisibleError(error);
+    } else if (visibleError) {
+      const timeout = setTimeout(() => setVisibleError(undefined), 1000);
+      return () => clearTimeout(timeout);
+    }
+  }, [error, visibleError]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setForm({ ...form, [event.target.name]: event.target.value });
+    if (error) {
+      setError(undefined);
+    }
   };
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
+    setError(undefined);
     await client.relayAsEmail(form)
       .catch((error: Error) => {
         console.error('Error sending message:', error);
+        setError('There was an error sending your message. Please try again later.');
       });
   };
 
   return (
-    <form onSubmit={handleSubmit} className="contact-form">
+    <form onSubmit={handleSubmit} className={
+      `contact-form
+      ${error ?
+        'shadow-2xl shadow-red-500/30' :
+        'shadow-2xl/45 shadow-amber-200/15'
+      }`
+    }>
+      <div className={
+        `contact-form-error
+        ${error ? 'opacity-100' : 'opacity-0 pointer-events-none'}`
+      }>
+        {visibleError}
+      </div>
       <FormField
         label="First Name"
         name="firstName"
